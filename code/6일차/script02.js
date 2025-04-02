742ccc8d14dfeec465649a798acbde40f0ca3bdf
dlqjsdp <dlqjsdp224@gmail.com>
2025-04-02 13:12:09 +0900

    20250402

---
+++ code/6일차/script02.js
@@ -0,0 +1,9 @@
+let score = 65;
+let grade = score >= 90 ? 'A' 
+            : score >= 80 ? 'B' 
+            : score >= 70 ? 'C'
+            : score >= 60 ? 'D'
+            : 'F';
+
+
+console.log(grade);
